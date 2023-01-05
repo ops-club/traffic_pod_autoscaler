c5de325caf54867c391a19b701b4f56e6886b779
Laurent RICHARD <laurent.richard@openclassrooms.com>
2023-01-06 00:54:23 +0100

    better error handler

--- traffic_pod_autoscaler/src/main.py
+++ traffic_pod_autoscaler/src/main.py
@@ -26,6 +26,8 @@ def parse_args():
                         dest="remote_address", required=True)
     parser.add_argument("--target-port", help="Target listen port",
                         dest="remote_port", type=int, default=80, required=False)
+    parser.add_argument("--target-timeout", help="Target listen timeout",
+                        dest="remote_timeout", type=int, default=120, required=False)
     parser.add_argument("--min-replicas", help="Number of replicas to start",
                         type=int, default=1, required=False)
 
@@ -88,7 +90,8 @@ def main():
         _httpserver = HTTPServer(_args, _proxy)
 
         _proxy.run()
-
+    except Exception as e:
+        _logger.exception(f"Exception:main_main:{e}")
     finally:
         _logger.info("STOP WATCHER")
         _httpserver.stop()
